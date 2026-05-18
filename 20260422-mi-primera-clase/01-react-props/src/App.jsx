9efc5391fc689b207408fd1d0861b31078b125b7
Esther López - Fundación Eurofirms <esther.lopez@fundacioneurofirms.org>
2026-05-18 20:04:35 +0200

    up all

--- 20260422-mi-primera-clase/01-react-props/src/App.jsx
+++ 20260422-mi-primera-clase/01-react-props/src/App.jsx
@@ -15,9 +15,9 @@ function App() {
     <>
       <h1>Hola, soy Esther    </h1>  
 
-      <Tarjeta src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsK7ox6ZUo_5spLsRJTAh2idvizo7QLqoJ-w&s" nombre="Esther" edad="37" />
-      <Tarjeta src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPVO4LSlzzN3vjB0C4LiWsqRG-iYFpJEvDHQ&s" nombre="Rossana" edad="46" />
-      <Tarjeta src="https://e7.pngegg.com/pngimages/101/287/png-clipart-computer-icons-nanny-template-job-desktop-avatar-woman-template-child.png" nombre="Amanda" edad="43" />
+      <Tarjeta src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsK7ox6ZUo_5spLsRJTAh2idvizo7QLqoJ-w&s" nombre="Esther" edad={37} />
+      <Tarjeta src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPVO4LSlzzN3vjB0C4LiWsqRG-iYFpJEvDHQ&s" nombre="Rossana" edad={46} />
+      <Tarjeta src="https://e7.pngegg.com/pngimages/101/287/png-clipart-computer-icons-nanny-template-job-desktop-avatar-woman-template-child.png" nombre="Amanda" edad={43} />
 
       <Tarjeta {...persona} />            
     </>
