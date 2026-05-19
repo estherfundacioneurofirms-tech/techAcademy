3cd16d3cebb280818984deb7ecfefdcc08e2d348
Esther López - Fundación Eurofirms <esther.lopez@fundacioneurofirms.org>
2026-05-19 21:12:09 +0200

    ejercicio Contexto Claro - Oscuro Completo

--- appThemeContext/src/App.jsx
+++ appThemeContext/src/App.jsx
@@ -1,13 +1,23 @@
 import { useState } from 'react'
+import { ThemeContext } from './context/ThemeContext'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import ThemeButton from './components/ThemeButton'
 import './index.css'
 
 function App() {
 
+  const [darkMode , setDarkMode] = useState(false)
 
   return (
-    <>
-
-    </>
+    <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
+      <div className={darkMode ? "app dark" : "app"}>
+        <Header />
+        <MainContent />
+        <ThemeButton />
+      </div>
+    </ThemeContext.Provider>
+   
   )
 }
 
--- appThemeContext/src/components/Header.jsx
+++ appThemeContext/src/components/Header.jsx
@@ -1,11 +1,12 @@
 import { useContext } from "react" 
+import { ThemeContext } from "../context/ThemeContext"
 
 
 function Header() {
     const { darkMode } = useContext(ThemeContext)
 
     return (
-        <header className={darkmode ? "dark" : ""}>
+        <header className={darkMode ? "dark" : ""}>
             <h1>Mi App Claro Oscuro</h1>
         </header>
     )
